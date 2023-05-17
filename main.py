479fcd545bb0224334ca3885aa938649e78f28da
GasparinoRocha <gasparsantosrocha@gmail.com>
2023-05-17 12:25:45 +0200

    Added metrics endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
@@ -26,15 +26,70 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.state.nPredictions = 0
+app.state.nWrongPredictions = 0
+app.state.reviewSize50Counter = 0
+app.state.reviewSize100Counter = 0
+app.state.reviewSize150Counter = 0
+app.state.reviewSize200Counter = 0
+app.state.reviewSize250Counter = 0
+app.state.reviewSizeInfCounter = 0
+app.state.reviewSizeSum = 0
+
 @app.post('/predict')
 async def predict(input_text: InputText):
     """
     Predicts the sentiment of a given text
     """
+    app.state.nPredictions += 1
     data = input_text.data
     processed_input = cv.transform([data]).toarray()[0]
     prediction = model.predict([processed_input])[0]
+
+    size = len(data)
+    if size <= 50:
+        app.state.reviewSize50Counter += 1
+    elif size <= 100:
+        app.state.reviewSize100Counter += 1
+    elif size <= 150:
+        app.state.reviewSize150Counter += 1
+    elif size <= 200:
+        app.state.reviewSize200Counter += 1
+    elif size <= 250:
+        app.state.reviewSize250Counter += 1
+    else:
+        app.state.reviewSizeInfCounter += 1
+
     return {'prediction': "Positive" if prediction == 1 else "Negative"}
+
+@app.get('/metrics')
+async def metrics():
+    """
+    Enables monitoring with Prometheus
+    """
+
+    global countIdx, countSub
+
+    m = "# HELP wrong_prediction_ratio Ratio of wrong predictions over all predictions.\n"
+    m+= "# TYPE wrong_prediction_ratio gauge\n"
+    m+= "wrong_prediction_ratio " + "0\n\n" if app.state.nPredictions == 0 else str(app.state.nWrongPredictions/app.state.nPredictions) + "\n\n"
+
+    m+= "# HELP num_requests The number of requests that have been served.\n"
+    m+= "# TYPE num_requests counter\n"
+    m+= "num_requests " + str(app.state.nPredictions) + "\n\n"
+
+    m+= "# HELP review_size A histogram of the review sizes (characters)\n"
+    m+= "# TYPE review_size histogram\n"
+    m+= "review_size_bucket{le=\"50\"}  " + str(app.state.reviewSize50Counter) + "\n"
+    m+= "review_size_bucket{le=\"100\"} " + str(app.state.reviewSize100Counter) + "\n"
+    m+= "review_size_bucket{le=\"150\"} " + str(app.state.reviewSize150Counter) + "\n"
+    m+= "review_size_bucket{le=\"200\"} " + str(app.state.reviewSize200Counter) + "\n"
+    m+= "review_size_bucket{le=\"250\"} " + str(app.state.reviewSize250Counter) + "\n"
+    m+= "review_size_bucket{le=\"+Inf\"} " + str(app.state.reviewSizeInfCounter) + "\n"
+    m+= "review_size_sum " + str(app.state.reviewSizeSum) + "\n"
+    m+= "review_size_count " + str(app.state.nPredictions) + "\n\n"
+
+    return Response(content=m, media_type="text/plain")
 
 if __name__ == '__main__':
     import uvicorn
