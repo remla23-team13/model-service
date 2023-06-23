49104982415664c9351cd5388d6e0d5c9b7713f2
Isa Rethans <isa.rethans@9to5.software>
2023-06-23 15:08:22 +0200

    add summary

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import os
 import urllib.request
 import gdown
 import restaurant_db
+import time
 from input_form import InputText
 
 gdown.download(os.getenv("MODEL_URL"), os.getenv("MODEL_FILE"), quiet=False)
-gdown.download(os.getenv("PREPROCESSOR_URL"), os.getenv("PREPROCESSOR_FILE"), quiet=False)
+gdown.download(os.getenv("PREPROCESSOR_URL"),
+               os.getenv("PREPROCESSOR_FILE"), quiet=False)
 
 model = joblib.load(os.getenv("MODEL_FILE"))
 preprocessor = joblib.load(os.getenv("PREPROCESSOR_FILE"))
@@ -36,29 +38,40 @@ app.state.reviewSize200Counter = 0
 app.state.reviewSize250Counter = 0
 app.state.reviewSizeInfCounter = 0
 app.state.reviewSizeSum = 0
+app.state.predictionTimeSum = 0
+app.state.predictionTimeCount = 0
+app.state.predictionTime1MicroSecond = 0
+app.state.predictionTime10MicroSecond = 0
+app.state.predictionTime100MicroSecond = 0
+app.state.predictionTimeSum1000MicroSecond = 0
+app.state.predictionTimeSum10000MicroSecond = 0
+
 
 @app.get('/restaurants')
 async def restaurants():
     """
     Returns a list of restaurants
     """
-    ## return the list as json
+    # return the list as json
     restaurants = restaurant_db.get_restaurants()
     return restaurants
+
 
 @app.get('/reviews/{restaurant_id}')
 async def reviews(restaurant_id: int):
     """
     Returns a list of reviews for a given restaurant
     """
-    ## return the list as json
+    # return the list as json
     reviews = restaurant_db.get_reviews(restaurant_id)
     return reviews
+
 
 @app.get('/restaurant/{restaurant_id}')
 async def restaurant(restaurant_id: int):
     restaurant = restaurant_db.get_restaurant(restaurant_id)
     return restaurant
+
 
 @app.post('/predict/{restaurant_id}')
 async def predict(input_text: InputText, restaurant_id: int):
@@ -66,10 +79,27 @@ async def predict(input_text: InputText, restaurant_id: int):
     Predicts the sentiment of a given text
     """
     app.state.nPredictions += 1
+    before = time.time()
     data = input_text.data
     processed_input = preprocessor.transform([data]).toarray()[0]
     prediction = model.predict([processed_input])[0]
+    after = time.time()
+    prediction_time = before - after
+
     restaurant_db.insert_review(restaurant_id, data)
+
+    app.state.predictionTimeCount += 1
+    if prediction_time <= 0.000001:
+        app.state.predictionTime1MicroSecond += 1
+    elif size <= 0.00001:
+        app.state.predictionTime10MicroSecond += 1
+    elif size <= 0.0001:
+        app.state.predictionTime100MicroSecond += 1
+    elif prediction_time <= 0.001:
+        app.state.predictionTime1000MicroSecond += 1
+    else:
+        app.state.predictionTime10000MicroSecond += 1
+    app.state.predictionTimeSum += prediction_time
 
     size = len(data)
     if size <= 50:
@@ -88,12 +118,14 @@ async def predict(input_text: InputText, restaurant_id: int):
 
     return {'prediction': "Positive" if prediction == 1 else "Negative"}
 
+
 @app.post('/wrong')
 async def wrong():
     """
     Increments the wrong prediction counter
     """
     app.state.nWrongPredictions += 1
+
 
 @app.get('/metrics')
 async def metrics():
@@ -106,31 +138,53 @@ async def metrics():
     version = os.getenv("VERSION")
 
     m = "# HELP wrong_prediction_ratio Ratio of wrong predictions over all predictions.\n"
-    m+= "# TYPE wrong_prediction_ratio gauge\n"
-    m+= "wrong_prediction_ratio "
-    
+    m += "# TYPE wrong_prediction_ratio gauge\n"
+    m += "wrong_prediction_ratio "
+
     if app.state.nPredictions == 0:
         m += "0\n\n"
     else:
         m += str(app.state.nWrongPredictions / app.state.nPredictions) + "\n\n"
 
+    m += "# HELP num_requests The number of requests that have been served.\n"
+    m += "# TYPE num_requests counter\n"
+    m += "num_requests{version=\"" + version + "\"} " + \
+        str(app.state.nPredictions) + "\n\n"  # TODO get version
 
-    m+= "# HELP num_requests The number of requests that have been served.\n"
-    m+= "# TYPE num_requests counter\n"
-    m+= "num_requests{version=\"" + version + "\"} " + str(app.state.nPredictions) + "\n\n" # TODO get version 
+    m += "# HELP review_size A histogram of the review sizes (characters)\n"
+    m += "# TYPE review_size histogram\n"
+    m += "review_size_bucket{le=\"50\"}  " + \
+        str(app.state.reviewSize50Counter) + "\n"
+    m += "review_size_bucket{le=\"100\"} " + \
+        str(app.state.reviewSize100Counter) + "\n"
+    m += "review_size_bucket{le=\"150\"} " + \
+        str(app.state.reviewSize150Counter) + "\n"
+    m += "review_size_bucket{le=\"200\"} " + \
+        str(app.state.reviewSize200Counter) + "\n"
+    m += "review_size_bucket{le=\"250\"} " + \
+        str(app.state.reviewSize250Counter) + "\n"
+    m += "review_size_bucket{le=\"+Inf\"} " + \
+        str(app.state.reviewSizeInfCounter) + "\n"
+    m += "review_size_sum " + str(app.state.reviewSizeSum) + "\n"
+    m += "review_size_count " + str(app.state.nPredictions) + "\n\n"
 
-    m+= "# HELP review_size A histogram of the review sizes (characters)\n"
-    m+= "# TYPE review_size histogram\n"
-    m+= "review_size_bucket{le=\"50\"}  " + str(app.state.reviewSize50Counter) + "\n"
-    m+= "review_size_bucket{le=\"100\"} " + str(app.state.reviewSize100Counter) + "\n"
-    m+= "review_size_bucket{le=\"150\"} " + str(app.state.reviewSize150Counter) + "\n"
-    m+= "review_size_bucket{le=\"200\"} " + str(app.state.reviewSize200Counter) + "\n"
-    m+= "review_size_bucket{le=\"250\"} " + str(app.state.reviewSize250Counter) + "\n"
-    m+= "review_size_bucket{le=\"+Inf\"} " + str(app.state.reviewSizeInfCounter) + "\n"
-    m+= "review_size_sum " + str(app.state.reviewSizeSum) + "\n"
-    m+= "review_size_count " + str(app.state.nPredictions) + "\n\n"
+    m += "# HELP prediction_time A summary of the time it takes to process the input and do a prediction\n"
+    m += "# TYPE prediction_time summary\n"
+    m += "prediction_time{quantile=\"0.00001\"}  " + \
+        str(app.state.predictionTime1MicroSecond) + "\n"
+    m += "prediction_time{quantile=\"0.0001\"} " + \
+        str(app.state.predictionTime10MicroSecond) + "\n"
+    m += "prediction_time{quantile=\"0.001\"} " + \
+        str(app.state.predictionTime100MicroSecond) + "\n"
+    m += "prediction_time{quantile=\"0.1\"} " + \
+        str(app.state.predictionTime1000MicroSecond) + "\n"
+    m += "prediction_time{quantile=\"1\"} " + \
+        str(app.state.predictionTime10000MicroSecond) + "\n"
+    m += "prediction_time_sum " + str(app.state.predictionTimeSum) + "\n"
+    m += "prediction_time_count " + str(app.state.predictionTimeCount) + "\n\n"
 
     return Response(content=m, media_type="text/plain")
+
 
 @app.get('/version')
 async def health():
@@ -142,4 +196,3 @@ async def health():
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
